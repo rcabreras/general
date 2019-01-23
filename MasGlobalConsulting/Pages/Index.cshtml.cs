cc572ce5aca9446906b739fc84aed6c9d1d87703
Raul Cabrera <raul.csa@gmail.com>
2019-01-22 22:47:51 -0500

    2

---
+++ MasGlobalConsulting/Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MasGlobalConsulting.Pages
+{
+    public class EmployeesModel : PageModel
+    {
+        public string Message { get; set; }
+        public List<Employee> _employees;
+
+        MasGlobalConsulting.Services.EmployeeService _employeeService;
+
+        public EmployeesModel() {
+            _employeeService = new Services.EmployeeService();
+        }
+
+
+        public void OnGet()
+        {
+            Message = "Your application description page.";
+        }
+        
+
+        [BindProperty]
+        public Employee employee{ get; set; }
+
+        public async Task<IActionResult> OnGetEmployeesById(String id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            this._employees = new List<Employee>();
+
+
+            if (String.IsNullOrEmpty(id))
+            {
+                this._employees.AddRange(await _employeeService.GetEmployees());
+            }
+            else {
+                this._employees.Add(await _employeeService.GetEmployee(id));
+            }
+
+            
+            
+
+            ViewData.Model = this;
+
+            return Page();
+        }
+
+    }
+}
---
+++ MasGlobalConsulting/Services/EmployeeService.cs
@@ -0,0 +1,47 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MasGlobalConsulting.Services
+{
+    public class EmployeeService
+    {
+        HttpClient _client = new HttpClient();
+
+        public EmployeeService()
+        {
+           
+            // Update port # in the following line.
+            _client.BaseAddress = new Uri("https://localhost:44335/");
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+        public async Task<Employee> GetEmployee(String _id)
+        {
+            var employees = await GetEmployees();
+            return employees.Find(x => x.id == _id);
+
+        }
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            List<Employee> emps = null;
+            HttpResponseMessage response = await _client.GetAsync("api/Employee");
+            
+            if (response.IsSuccessStatusCode)
+            {
+                emps = await response.Content.ReadAsAsync<List<Employee>>();
+            }
+
+            return emps;
+        }
+
+    }
+}
